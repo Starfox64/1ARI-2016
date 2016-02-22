19f69897f84bcac1bd38c40d58f1b3e9804a2b04
Eirheinger <orban.valentin@gmail.com>
2016-02-22 14:40:50 +0100

    began project: chiffreDuLivre has now all crypt/decrypt methods, onl parse methods missing

---
+++ src/chiffreDuLivre.py
@@ -0,0 +1,47 @@
+from random import randint
+
+
+def createDict(keyText, size):
+    dico = dict()
+    fakeOccurence = size
+    for i in range(65, 91):
+        key = chr(i)
+        l = list()
+        isInText = False
+
+        for j in range(size):
+            if keyText[j] == key:
+                isInText = True
+                l.append(j)
+
+        if isInText is False:
+            l.append(fakeOccurence)
+        dico[key] = l.copy()
+        fakeOccurence += 1
+    return dico
+
+
+def encryptLetter(letter, key):
+    x = randint(0, len(key[letter]))
+    return key[letter][x]
+
+
+def encryptText(text, size, key):
+    l = list()
+    for i in range(size):
+        l.append(encryptLetter(text[i], key))
+    return l
+
+
+def decryptLetter(index, key):
+    for i in range(65, 91):
+        if index in key[chr(i)]:
+            return chr(i)
+    return 0
+
+
+def decryptText(cryptedText, size, key):
+    clearText = list()
+    for i in range(size):
+        clearText.append(decryptLetter(cryptedText[i], key))
+    return clearText
